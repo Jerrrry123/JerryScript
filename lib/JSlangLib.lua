642473c5d1a2017eadef2088bdbd1989d371280d
Jerry <jhonson.henry69@gmail.com>
2022-06-08 13:05:31 +0200

    FIx langLib

--- lib/JSlangLib.lua
+++ lib/JSlangLib.lua
@@ -2,8 +2,9 @@ local function getPathPart(fullPath, remove)
     local path = string.sub(fullPath, #remove + 1)
     return string.gsub(path, '.lua', '')
 end
+require('store\\JerryScript\\Language\\english')
 for _, profilePath in pairs(filesystem.list_files(filesystem.store_dir() ..'JerryScript\\Language')) do
-    if string.find(profilePath, 'template') == nil then
+    if string.find(profilePath, 'english') == nil and string.find(profilePath, 'template') == nil then
         require(getPathPart(profilePath, filesystem.scripts_dir()))
     end
 end
