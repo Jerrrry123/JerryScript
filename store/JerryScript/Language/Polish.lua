15fa06e5dd0898ce15c60a8be32ea5ac85feae8b
Jerry <104687980+Jerrrry123@users.noreply.github.com>
2023-04-13 03:33:17 +0200

    Update Polish.lua

--- store/JerryScript/Language/Polish.lua
+++ store/JerryScript/Language/Polish.lua
@@ -1,10 +1,7 @@
 lang.set_translate('pl') --thank you DumbBird#9143 for creating this translation ^-^
 
 local f = lang.find_registered
-local t = function(registered_label, translated_text)
-  if (registered_label == 0) then return end
-  lang.translate(registered_label, translated_text)
-end
+local t = lang.translate
 
 t(f('Failed to get latest release.'), 'Nie udało się pobrać najnowszej wersji.')
 t(f('Notifications on'), 'Włącz powiadomienia')
