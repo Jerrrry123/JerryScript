a1df5c8743c8788955662994e3f307028e9a6efe
Jerry <104687980+Jerrrry123@users.noreply.github.com>
2023-04-13 02:59:36 +0200

    Update Lithuanian.lua

--- store/JerryScript/Language/Lithuanian.lua
+++ store/JerryScript/Language/Lithuanian.lua
@@ -1,7 +1,10 @@
 lang.set_translate('lt')
 
-local f = lang.find
-local t = lang.translate
+local f = lang.find_registered
+local t = function(registered_label, translated_text)
+  if (registered_label == 0) then return end
+  lang.translate(registered_label, translated_text)
+end
 
 t(f('Failed to get latest release.'), 'Nepavyko gauti naujausio leidimo.')
 t(f('Notifications on'), 'Pranešimai įjungti')
