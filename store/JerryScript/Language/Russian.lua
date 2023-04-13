1503230284df04602c14b9c2a1edede0e1b7e561
Jerry <104687980+Jerrrry123@users.noreply.github.com>
2023-04-13 03:34:18 +0200

    Update Russian.lua

--- store/JerryScript/Language/Russian.lua
+++ store/JerryScript/Language/Russian.lua
@@ -1,10 +1,7 @@
 lang.set_translate('ru')
 
 local f = lang.find_registered
-local t = function(registered_label, translated_text)
-  if (registered_label == 0) then return end
-  lang.translate(registered_label, translated_text)
-end
+local t = lang.translate
 
 t(f('Failed to get latest release.'), 'Не удалось получить последнюю версию.')
 t(f('Notifications on'), 'Уведомления о')
