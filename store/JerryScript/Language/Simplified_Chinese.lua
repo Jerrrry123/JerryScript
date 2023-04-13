86838dc97f57ff879d37dacc6f6b85af719a6fcc
Jerry <104687980+Jerrrry123@users.noreply.github.com>
2023-04-13 03:34:45 +0200

    Update Simplified_Chinese.lua

--- store/JerryScript/Language/Simplified_Chinese.lua
+++ store/JerryScript/Language/Simplified_Chinese.lua
@@ -1,10 +1,7 @@
 lang.set_translate('zh') --thank you zzzz#5116 for creating this translation ^-^et location
 
 local f = lang.find_registered
-local t = function(registered_label, translated_text)
-  if (registered_label == 0) then return end
-  lang.translate(registered_label, translated_text)
-end
+local t = lang.translate
 
 t(f('Failed to get latest release.'), '获取最新版本失败')
 t(f('Notifications on'), '通知已开启')
